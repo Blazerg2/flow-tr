b67ed061820076424681e5ff5196303747cfb9b8
Blazerg <tnii.gerson@gmail.com>
2018-06-30 19:17:17 +0200

    updated page model2

--- page.go
+++ page.go
@@ -1,10 +1,15 @@
 package main
 
 type Page struct {
-	Text      string   `json:"text"`
-	InState   int      `json:"intStates"`
-	Character string   `json:"character"`
-	Decisions []string `json:"decisions"`
+	Text      string     `json:"text"`
+	InState   int        `json:"intStates"`
+	Character string     `json:"character"`
+	Decisions []Decision `json:"decisions"`
+}
+
+type Decision struct {
+	Code        int    `json:"code"`
+	Description string `json:"description"`
 }
 
 type Pages []Page
