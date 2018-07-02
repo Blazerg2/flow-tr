ff95d8f01add4505763859580f318b351a90ce66
Miguel Savignano <migue.masx@gmail.com>
2018-07-03 01:30:29 +0200

    me rindo

--- main.go
+++ main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"fmt"
-	// "net/http"
+	"net/http"
 	// "log"
-	// "github.com/gorilla/mux"
-	// "encoding/json"
+	"github.com/gorilla/mux"
+	"encoding/json"
 	"os"
 	"gopkg.in/mgo.v2"
 	// "crypto/tls"
@@ -13,24 +13,36 @@ import (
 )
 
 func main() {
-	var mongoURI = os.Getenv("MONGO_URI")
-	// var collection = getSession().DB(os.Getenv("MONGO_DATABASE")).C("pages")
-	session, err := mgo.Dial(mongoURI)
-	collection := session.DB("test").C("pages")
-	fmt.Println("%s", err)
-	fmt.Println("%s", session)
-	fmt.Println("collection %s", collection)
-	var results []Page
-	errr := collection.Find(nil).All(&results)
-	if errr != nil {
-		panic(errr)
-	}
-	fmt.Println("page %s", results)
 	// router := NewRouter()
-	// port := GetPort()
-	// server := http.ListenAndServe(port, router)
-	// fmt.Sprintf("Server start %s", port )
+	port := ":4747"
+	fmt.Println("Server start %s", port )
+	http.HandleFunc("/pages", GetPagesList)
+	// server := http.ListenAndServe(port, nil)
+	fmt.Println("Server start %s", port )
+	if err := http.ListenAndServe(port, nil); err != nil {
+    panic(err)
+  }
 	// log.Fatal(server)
+}
+
+func MongoRepo() *mgo.Database {
+	var mongoURI = os.Getenv("MONGO_URI")
+	session, err := mgo.Dial(mongoURI)
+	if err != nil {
+		panic(err)
+	}
+	database := session.DB("test")
+	return database
+}
+
+func AllPages() ([]Page, error) {
+	var results []Page
+	collection := MongoRepo().C("pages")
+	err := collection.Find(nil).All(&results)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	return results, err
 }
 
 // var mongoURI = os.Getenv("MONGO_URI")
@@ -64,18 +76,16 @@ func main() {
 // 	fmt.Fprintf(w, params["pageVarId"])
 // }
 
-// func GetPagesList(w http.ResponseWriter, r *http.Request) {
-// 	var results []Page
-// 	err := collection.Find(nil).All(&results)
-
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	w.Header().Set("Content-Type", "application/json")
-// 	w.WriteHeader(200)
-// 	json.NewEncoder(w).Encode(results)
-// }
+func GetPagesList(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Request /pages")
+	var results, err = AllPages()
+	if err != nil {
+		panic(err)
+	}
+	// w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	json.NewEncoder(w).Encode(results)
+}
 
 // func PostPagesList(w http.ResponseWriter, r *http.Request) {
 // 	decoder := json.NewDecoder(r.Body)
@@ -100,60 +110,86 @@ func main() {
 // }
 
 // // Get the Port from the environment so we can run on Heroku
-// func GetPort() string {
-// 	var port = os.Getenv("PORT")
-// 	// Set a default port if there is nothing in the environment
-// 	if port == "" {
-// 		port = "4747"
-// 		fmt.Println("INFO: No PORT environment variable detected, defaulting to " + port)
+func GetPort() string {
+	var port = os.Getenv("PORT")
+	// Set a default port if there is nothing in the environment
+	if port == "" {
+		port = "4747"
+		fmt.Println("INFO: No PORT environment variable detected, defaulting to " + port)
+	}
+	return ":" + port
+}
+
+type Route struct {
+	Name        string
+	Method      string
+	Pattern     string
+	HandlerFunc http.HandlerFunc
+}
+
+type Routes []Route
+
+func NewRouter() *mux.Router {
+	router := mux.NewRouter().StrictSlash(true)
+
+	for _, route := range routes {
+		router.Methods(route.Method).Path(route.Pattern).Name(route.Name).Handler(route.HandlerFunc)
+	}
+
+	return router
+}
+
+var routes = Routes{
+	// Route{
+	// 	"Index",
+	// 	"GET",
+	// 	"/",
+	// 	Index,
+	// },
+	// Route{
+	// 	"GetPageVar",
+	// 	"GET",
+	// 	"/pagevar/{pageVarId}",
+	// 	GetPageVar,
+	// },
+	Route{
+		"GetPagesList",
+		"GET",
+		"/pages",
+		GetPagesList,
+	},
+	// Route{
+	// 	"PostPagesList",
+	// 	"POST",
+	// 	"/pages",
+	// 	PostPagesList,
+	// },
+}
+
+// func MongoRepo() {
+// 	// 	dialInfo, err := mgo.ParseURL(mongoURI)
+// 	// 	if (err != nil) {
+// 	// 		panic(err)
+// 	// 	}
+// 	// 	tlsConfig := &tls.Config{}
+// 	// 	dialInfo.DialServer = func(addr *mgo.ServerAddr) (net.Conn, error) {
+// 	// 		conn, err := tls.Dial("tcp", addr.String(), tlsConfig)
+// 	// 		return conn, err
+// 	// 	}
+// 	// 	session, err := mgo.DialWithInfo(dialInfo)
+// 	// 	if (err != nil) {
+// 	// 		panic(err)
+// 	// 	}
+// 	// 	log.Println("MongoDB cluster connected")
+// 	// 	return session
+
+// 	var mongoURI = os.Getenv("MONGO_URI")
+// 	session, err := mgo.Dial(mongoURI)
+// 	if errr != nil {
+// 		panic(errr)
 // 	}
-// 	return ":" + port
-// }
-
-// type Route struct {
-// 	Name        string
-// 	Method      string
-// 	Pattern     string
-// 	HandlerFunc http.HandlerFunc
-// }
-
-// type Routes []Route
-
-// func NewRouter() *mux.Router {
-// 	router := mux.NewRouter().StrictSlash(true)
-
-// 	for _, route := range routes {
-// 		router.Methods(route.Method).Path(route.Pattern).Name(route.Name).Handler(route.HandlerFunc)
-// 	}
-
-// 	return router
-// }
-
-// var routes = Routes{
-// 	Route{
-// 		"Index",
-// 		"GET",
-// 		"/",
-// 		Index,
-// 	},
-// 	Route{
-// 		"GetPageVar",
-// 		"GET",
-// 		"/pagevar/{pageVarId}",
-// 		GetPageVar,
-// 	},
-// 	Route{
-// 		"GetPagesList",
-// 		"GET",
-// 		"/pages",
-// 		GetPagesList,
-// 	},
-// 	Route{
-// 		"PostPagesList",
-// 		"POST",
-// 		"/pages",
-// 		PostPagesList,
-// 	},
+// 	database := session.DB("test")
+// 	return database
 // }
 
 
